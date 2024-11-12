a1ca0a053776a17657cf3a97d35c477415408b0d
lujingfeng <lujf@guoguokeji.com>
2024-11-12 23:14:38 +0800

    add

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -1,5 +1,20 @@
 import { useState } from 'react';
 
+interface LinkListNode {
+  data: any;
+  next?: LinkListNode
+}
+
+const quickSort = ()=>{
+
+}
+const mergeSort = ()=>{
+  const a = [4, 2, 5, 12, 3];
+  const pivot = a.length - 1;
+  
+}
+
+
 export default function IndexPage() {
   const [count, setCount] = useState(1);
   const updateCount = () => setCount((c) => c + 1);
